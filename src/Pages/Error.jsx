3540905dbdab67fe9c3a22fcf5aa4d498e54d220
jazeel5 <mohammedjazeel5@gmail.com>
2025-03-01 11:51:27 +0530

    updated error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Error() {
   return (
     <div>
-
       <img src="https://cdn.dribbble.com/userupload/24450589/file/original-7a69eb5b87401ce59325c3291535aebc.gif" alt="" />
-      
     </div>
   );
 }
