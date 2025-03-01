f0f4b9465712ea3eaa3744fb8a7e3615006ad622
jazeel5 <mohammedjazeel5@gmail.com>
2025-03-01 10:44:49 +0530

    I have created contact.jsx file

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -5,6 +5,7 @@ export default function Error() {
     <div>
 
       <img src="https://cdn.dribbble.com/userupload/24450589/file/original-7a69eb5b87401ce59325c3291535aebc.gif" alt="" />
+      
     </div>
   );
 }
